2c44d4ed39db22d1c6395c578fb199402c9d9c55
Kelv1nG <kelvinmaglipon@gmail.com>
2024-08-24 00:24:25 +0800

    create subject button

---
+++ lib/modules/subjects/views/widgets/add_subject_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flash_cards/modules/subjects/bloc/subjects_bloc.dart';
+import './subject_form_dialog.dart';
+
+class AddSubjectButton extends StatelessWidget {
+  const AddSubjectButton({super.key});
+
+  static const String label = 'Add Subject';
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton.extended(
+      label: const Text(label),
+      onPressed: () async {
+        _onPressed(context);
+      },
+      icon: const Icon(Icons.add),
+      foregroundColor: Colors.white,
+      backgroundColor: Colors.pink,
+    );
+  }
+
+  void _onPressed(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (_) {
+        return BlocProvider.value(
+          value: context.read<SubjectsBloc>(),
+          child: SubjectFormDialog(
+            title: label,
+            onSubmit: (name) {
+              context.read<SubjectsBloc>().add(SubjectAddedEvent(name));
+            },
+          ),
+        );
+      },
+    );
+  }
+}
