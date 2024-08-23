a9ad59342d34ef4247055a0785659f0b4d0d36f4
Kelv1nG <kelvinmaglipon@gmail.com>
2024-08-24 00:24:45 +0800

    create subject form page

---
+++ lib/modules/subjects/views/widgets/subject_form.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flash_cards/models/subject.dart';
+
+class SubjectFormPage extends StatelessWidget {
+  SubjectFormPage({super.key, this.subject, required this.onSubmit})
+      : _controller = TextEditingController(text: subject?.name);
+
+  final Subject? subject;
+  final Function onSubmit;
+  final TextEditingController _controller;
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 32, right: 32),
+            child: Card(
+              child: TextFormField(autofocus: true, controller: _controller),
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: ElevatedButton(
+                onPressed: () {
+                  onSubmit(_controller.text);
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.pink,
+                ),
+                child: const Text("Save"),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
