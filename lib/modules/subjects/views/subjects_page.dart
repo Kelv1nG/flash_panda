d83b965ca27e6ee0b98dd138cfa2f39b06b5b5fa
Kelv1nG <kelvinmaglipon@gmail.com>
2024-08-24 12:37:45 +0800

    add snackbar notification

--- lib/modules/subjects/views/subjects_page.dart
+++ lib/modules/subjects/views/subjects_page.dart
@@ -1,6 +1,6 @@
-import "package:flash_cards/modules/subjects/subjects.dart";
 import "package:flutter/material.dart";
 import "package:flutter_bloc/flutter_bloc.dart";
+import "package:flash_cards/modules/subjects/subjects.dart";
 
 class SubjectsPage extends StatelessWidget {
   const SubjectsPage({super.key});
@@ -26,17 +26,38 @@ class SubjectsView extends StatelessWidget {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.miniEndTop,
       floatingActionButton: const AddSubjectButton(),
-      body: BlocBuilder<SubjectsBloc, SubjectsState>(
-        builder: (context, subjectsState) {
-          return ListView.builder(
-            itemCount: subjectsState.subjects.length,
-            itemBuilder: (context, index) {
-              final subject = subjectsState.subjects[index];
-              return SubjectCard(subject: subject);
-            },
-          );
+      body: BlocListener<SubjectsBloc, SubjectsState>(
+        listenWhen: (previous, current) =>
+            previous.subjectList != current.subjectList &&
+            current.action != null,
+        listener: (context, state) {
+          final messenger = ScaffoldMessenger.of(context);
+          messenger.showSnackBar(_notificationBar(state));
         },
+        child: BlocBuilder<SubjectsBloc, SubjectsState>(
+          builder: (context, subjectsState) {
+            return ListView.builder(
+              itemCount: subjectsState.subjectList.length,
+              itemBuilder: (context, index) {
+                final subject = subjectsState.subjectList[index];
+                return SubjectCard(subject: subject);
+              },
+            );
+          },
+        ),
       ),
     );
+  }
+
+  SnackBar _notificationBar(SubjectsState state) {
+    final subjectName = state.subject?.name ?? '';
+    final actionTaken = state.action?.name;
+    final displayText = "Subject $subjectName $actionTaken";
+    final snackBar = SnackBar(
+      content: Text(displayText, style: const TextStyle(color: Colors.white)),
+      backgroundColor: Colors.pink,
+      duration: const Duration(seconds: 1),
+    );
+    return snackBar;
   }
 }
