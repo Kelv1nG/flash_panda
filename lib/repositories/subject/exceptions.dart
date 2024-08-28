8dcafc83fd024a93252947fd6f1f72604e6e0ce3
Kelv1nG <kelvinmaglipon@gmail.com>
2024-08-28 11:28:03 +0800

    exceptions

---
+++ lib/repositories/subject/exceptions.dart
@@ -0,0 +1,9 @@
+class SubjectNotFound implements Exception {}
+
+class SubjectRequestFailed implements Exception {}
+
+class SubjectAddFailed implements Exception {}
+
+class SubjectUpdateFailed implements Exception {}
+
+class SubjectDeleteFailed implements Exception {}
