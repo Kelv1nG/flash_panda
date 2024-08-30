5d8701e2412330376076e4aebcaab81a4a2b7650
Kelv1nG <kelvinmaglipon@gmail.com>
2024-08-30 15:10:56 +0800

    change directory

--- lib/repositories/subject/exceptions.dart
+++
@@ -1,9 +0,0 @@
-class SubjectNotFound implements Exception {}
-
-class SubjectRequestFailed implements Exception {}
-
-class SubjectAddFailed implements Exception {}
-
-class SubjectUpdateFailed implements Exception {}
-
-class SubjectDeleteFailed implements Exception {}
--- lib/repositories/subject/subject.dart
+++
@@ -1,4 +0,0 @@
-export 'models/subject.dart';
-export 'subject_repository_local.dart';
-export 'subject_repository_remote.dart';
-
